52810ae6ae4e1f65595384bd5ec43e8e638e1b79
池田充宏 <ikedamitsuhiro@ikedamitsuhironoMacBook-Air.local>
2017-08-09 21:54:16 +0900

    second kauai

---
+++ SecondKadaiApp/ResultViewController.swift
@@ -0,0 +1,38 @@
+//
+//  ResultViewController.swift
+//  SecondKadaiApp
+//
+//  Created by 池田充宏 on 2017/08/08.
+//  Copyright © 2017年 池田hiroya. All rights reserved.
+//
+
+import UIKit
+
+class ResultViewController: UIViewController {
+
+    @IBOutlet weak var label: UILabel!
+    var text:String = ""
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        label.text = text
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
